2277a61a8f86d8b50b677cbff8d6614502575c30
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-08 15:25:23 -0500

    set up Root component and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './components/Root';
+import Home from './pages/Home';
+
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+export default function Navbar() {
+  return (
+    <nav className="text-white bg-black h-20 fixed top-0 w-screen flex justify-between items-center px-8">
+      <FontAwesomeIcon icon={faBars} className=""></FontAwesomeIcon>
+
+      <button className="font-heading">
+        <FontAwesomeIcon
+          icon={faCartShopping}
+          className="mr-2"
+        ></FontAwesomeIcon>
+        Cart
+      </button>
+    </nav>
+  );
+}
