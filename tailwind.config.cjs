661f931a9c60a6bece08b2f1967c1eb0bdc647a4
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-08 18:38:16 -0500

    custom styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,37 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: 'rgb(168, 134, 77)',
+      },
+      backgroundImage: {
+        header: "url('/assets/header-bg.jpg')",
+      },
+      width: {
+        aboutImg: '300px',
+        menuImg: '70px',
+        desktopMenuImg: '100px',
+        instagram: '250px',
+      },
+      height: {
+        logo: '120px',
+        menuImg: '70px',
+        desktopMenuImg: '100px',
+        instagram: '250px',
+      },
+      fontFamily: {
+        heading: ['Poppins'],
+      },
+      borderWidth: {
+        line: '0.5px',
+        button: '1px',
+      },
+      boxShadow: {
+        btnHover: '0 10px 20px rgba(0, 0, 0, 0.2)',
+        btnActive: '0 5px 10px rgba(0, 0, 0, 0.2)',
+      },
+    },
   },
   plugins: [],
 };
