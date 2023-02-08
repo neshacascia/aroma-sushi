18467248cb71d46f4119254f9aef17b104c72732
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-08 17:37:05 -0500

    feat: added hover states to buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
 export default function Button(props) {
   return (
     <button
-      className={`font-display tracking-wider font-light bg-${props.bg} text-${
-        props.colour
-      } ${props.border ? 'border-black border-2' : undefined} px-8 py-4`}
+      className={`${
+        props.border && props.hover
+          ? `text-black bg-white border-black border-2 after:bg-black hover:text-white`
+          : props.border
+          ? `text-black bg-white border-black border-2`
+          : `bg-black text-white after:bg-white hover:text-black hover:border-2 hover:border-black`
+      } px-8 py-4 btn-fade`}
     >
       {props.text}
     </button>
