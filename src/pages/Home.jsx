e9861877243d4afa0e2e77ad3df87ba360ee632f
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-08 15:42:29 -0500

    feat: created section for restaurant's opening times

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,6 +46,32 @@ export default function Home() {
 
       <hr />
       <FeaturedMenuItems />
+      <hr />
+
+      <section className="font-heading h-full px-6 my-10">
+        <div className="bg-black text-white flex flex-col items-center gap-4 py-10 ">
+          <h3 className="font-bold text-2xl tracking-widest mb-5">
+            OPENING TIMES
+          </h3>
+          <div className="flex items-center">
+            <span>Tuesdays - Sundays</span>
+            <hr className="border-line w-6 mx-4" />
+            <span>11:00 - 22:00</span>
+          </div>
+
+          <div className="flex items-center mb-5">
+            <span>Mondays</span>
+            <hr className="border-line w-6 mx-4" />
+            <span>Closed</span>
+          </div>
+
+          <span className="text-center font-bold text-lg tracking-wider mb-5">
+            BOOK YOUR TABLE FOR <br /> LUNCH OR DINNER
+          </span>
+
+          <Button bg="white" colour="black" text="RESERVE NOW" />
+        </div>
+      </section>
     </section>
   );
 }
