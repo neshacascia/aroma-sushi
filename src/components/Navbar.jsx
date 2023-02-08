8f7bc1446d5f0e13233ab65cbed94c4555194b5f
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-08 17:38:12 -0500

    fix: set z-index to 10

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
   return (
-    <nav className="text-white bg-black h-20 fixed top-0 w-screen flex justify-between items-center px-8">
+    <nav className="text-white bg-black h-20 fixed top-0 z-10 w-screen flex justify-between items-center px-8">
       <FontAwesomeIcon icon={faBars} className=""></FontAwesomeIcon>
 
       <button className="font-heading">
