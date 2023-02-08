e39cbe341f7b5aa73b1aa6e94d7f1f73ee042c01
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-08 15:27:07 -0500

    installed font awesome icons and set up arrays to map over data

---
+++ data.js
@@ -0,0 +1,48 @@
+import {
+  faLocationDot,
+  faCalendar,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faSquareInstagram,
+  faSquareFacebook,
+  faSquareTwitter,
+  faSquareYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+
+export const aboutImagesArr = [
+  '/assets/about/about-1.jpg',
+  '/assets/about/about-2.jpg',
+  '/assets/about/about-3.jpg',
+];
+export const footerData = [
+  {
+    id: 'i1',
+    icon: faLocationDot,
+    heading: 'Address',
+    line1: '123 Yonge St.',
+    line2: 'Toronto, Canada',
+  },
+  {
+    id: 'i2',
+    icon: faCalendar,
+    heading: 'Reservations',
+    line1: '+1 123-456-7890',
+    line2: 'b**karoma@aromasushi.com',
+  },
+  {
+    id: 'i3',
+    icon: faClock,
+    heading: 'Opening Hours',
+    line1: 'Tue - Sun: 11:00 - 22:00',
+    line2: 'Mon: Closed',
+  },
+];
+
+export const brandIcons = [
+  faSquareInstagram,
+  faSquareFacebook,
+  faSquareTwitter,
+  faSquareYoutube,
+];
