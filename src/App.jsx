fccb3df4722592eb8756b1f440368628c88001e5
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-15 23:23:06 -0500

    convert to router components

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,37 @@
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import { StateContext } from './context/StateContext';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  MemoryRouter,
-} from 'react-router-dom';
+import { Routes, Route, MemoryRouter } from 'react-router-dom';
 import Root from './components/Root';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import About from './pages/About';
+
 import MobileMenuModal from './components/modals/MobileMenuModal';
 import CartModal from './components/modals/CartModal';
 
 import './App.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [{ index: true, element: <Home /> }],
-  },
-]);
 
 function App() {
   const { isClicked, isCartOpen } = useContext(StateContext);
 
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/about" element={<About />}></Route>
+      </Routes>
 
       {isClicked &&
         ReactDOM.createPortal(
-          <MemoryRouter>
-            <MobileMenuModal />
-          </MemoryRouter>,
+          <MobileMenuModal />,
           document.getElementById('mobile-menu-modal-root')
         )}
 
       {isCartOpen &&
         ReactDOM.createPortal(
-          <MemoryRouter>
-            <CartModal />
-          </MemoryRouter>,
+          <CartModal />,
           document.getElementById('cart-modal-root')
         )}
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { StateContextProvider } from './context/StateContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <StateContextProvider>
-      <App />
-    </StateContextProvider>
-  </React.StrictMode>
+  <StateContextProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </StateContextProvider>
 );
