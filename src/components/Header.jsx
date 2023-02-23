9d40b2f3aaf9beba3e6481e49ad37ebc28dec784
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-22 19:28:17 -0500

    style: removed extra whitespace

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-header  bg-center bg-cover bg-fixed h-screen text-white pt-20">
+    <header className="bg-header bg-center bg-cover bg-fixed h-screen text-white pt-20">
       <div className="bg-black/30 h-full font-heading flex flex-col items-center justify-center text-center">
         <h1 className="text-4xl font-bold px-4 py-4 mb-4">
           Enjoy modern <br />
